ba67ec0e0ce66b4538eeeed1442a15743eb1a31a
isKarl <isKarl@126.com>
2021-09-08 20:40:46 +0800

    add java01

---
+++ tool/src/main/java/com/cold/param/RedisParamenter.java
@@ -0,0 +1,9 @@
+package com.cold.param;
+
+public class RedisParamenter {
+
+    public  static  final  String ADMIN_LOING_USER_REDIS_KEY = "adminKey";
+
+    //用户登陆rediskey
+    public  static  final  String USER_REDIS_LOGIN_KEY = "user:login";
+}
